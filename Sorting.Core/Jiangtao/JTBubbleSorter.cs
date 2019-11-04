89f6e061dd63b58faf158880d6110a095144e70a
jiangtao <457296124@qq.com>
2019-11-04 18:58:00 +0800

    BubbleSorter

---
+++ Sorting.Core/Jiangtao/JTBubbleSorter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Jiangtao
+{
+    public class JTBubbleSorter : SorterBase
+    {
+        public override void Sort(int[] input)
+        {
+            if (input == null)
+            {
+                return;
+            }
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+
+                for (int j = 0; j < input.Length - i - 1; j++)
+                {
+                    int temp = 0;
+                    if (input[j] > input[j + 1])
+                    {
+                        temp = input[j];
+                        input[j] = input[j + 1];
+                        input[j + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
