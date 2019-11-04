b83cca6ddef8ef26947dc1e88680e3a4038e9b16
Wuqi0957 <1393153202@qq.com>
2019-11-04 16:35:50 +0800

    MergerSort QuickSorte SelectionSort

---
+++ Sorting.Core/Wuqi/WQQuickSorter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Wuqi
+{
+    class WQQuickSorter : SorterBase
+    {
+        public override void Sort(int[] input)
+        {
+            if (input == null)
+            {
+                return;
+            }
+            quickSort(0, input.Length, input);
+        }
+        static void quickSort(int bgn, int end, params int[] arr)
+        {
+
+            if (bgn >= end - 1)
+                return;
+            int lindex = bgn;
+            int rindex = end - 1;
+            int std = arr[lindex];
+            while (lindex < rindex)
+            {
+                while (lindex < rindex)
+                {
+                    if (arr[rindex] < std)
+                    {
+                        arr[lindex++] = arr[rindex];
+                        break;
+                    }
+                    --rindex;
+
+                }
+
+                while (lindex < rindex)
+                {
+                    if (arr[lindex] >= std)
+                    {
+                        arr[rindex--] = arr[lindex];
+                        break;
+                    }
+                    ++lindex;
+
+                }
+
+            }
+            arr[lindex] = std;
+            quickSort(bgn, lindex, arr);
+            quickSort(rindex + 1, end, arr);
+        }
+    }
+}
