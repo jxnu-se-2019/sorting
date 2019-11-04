fbdd463eafbad36949eca435b3826e5f22ee14ab
Xu-Qiao <2297770820@qq.com>
2019-11-04 17:05:13 +0800

    XQHeapSorter

---
+++ Sorting.Core/Xuqiao/XQHeapSorter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Xuqiao
+{
+    class XQHeapSorter : SorterBase
+    {
+
+        public override void Sort(int[] b)
+        {
+            if (b == null)
+            {
+                return;
+            }
+            int maxid = GetMax(b.Length, b);
+            HeapSort1(maxid, b);
+        }
+
+        public static void Swap(int maxIdx, int idx, int[] lst)
+        {
+            int midx = maxIdx;
+            int temp = lst[idx];
+            lst[idx] = lst[maxIdx];
+            lst[maxIdx] = temp;
+        }
+        public static int GetMax(int maxLen, int[] lst)
+        {
+            int maxIdx = 0;
+            for (var i = 0; i < maxLen; i++)
+            {
+                if (lst[i] > lst[maxIdx])
+                    maxIdx = i;
+            }
+
+            return maxIdx;
+        }
+        public static IList<int> HeapSort1(int maxIdx, int[] lst)
+        {
+            int i = lst.Length - 1;
+            int k = 0;
+
+            while (i > -1)
+            {
+                Swap(maxIdx, i, lst);
+                k++;
+                maxIdx = GetMax(lst.Length - k, lst);
+                i--;
+            }
+
+            return lst;
+        }
+    }
+}
