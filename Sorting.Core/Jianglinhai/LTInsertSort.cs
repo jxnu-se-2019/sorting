1e3234343c5decc8a215ffe7353981b8fd84d9f3
jiang-linhai <1345557381@qq.com>
2019-11-05 11:23:57 +0800

    jainglinhai-liutian_sort

---
+++ Sorting.Core/Jianglinhai/LTInsertSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Jianglinhai
+{
+    class LTInsertSort : SorterBase
+    {
+        public override void Sort(int[] input)
+        {
+            if (input == null)
+            {
+                return;
+            }
+            insert(input);
+
+        }
+        public static void insert(int[] a)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (a[j] > a[j - 1]) break;
+                    else
+                    {
+                        int sap = a[j];
+                        a[j] = a[j - 1];
+                        a[j - 1] = sap;
+                    }
+                }
+            }
+        }
+    }
+}
