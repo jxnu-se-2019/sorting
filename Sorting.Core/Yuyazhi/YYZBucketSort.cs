8dbedbc76a1f3554657ecf4c90f65c0082723268
YYZJ <2271440673@qq.com>
2019-11-11 08:51:49 +0800

    QuickSort and BucketSort

---
+++ Sorting.Core/Yuyazhi/YYZBucketSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Yuyazhi
+{
+    public class YYZBucketSort:SorterBase
+    {
+        public override void Sort(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+            BucketSort(input);
+        }
+        private static void BucketSort(int[] input)
+        {
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            for(int i=0;i<input.Length;i++)
+            {
+                max = Math.Max(max, input[i]);
+                min = Math.Min(min, input[i]);
+            }
+            int bucketnum = (max - min) / input.Length + 1;
+            List<List<int>> bucketinput = new List<List<int>>(bucketnum);
+            for(int i=0;i<bucketnum;i++)
+            {
+                bucketinput.Add(new List<int>());
+            }
+            for(int i=0;i<input.Length;i++)
+            {
+                int num = (input[i] - min) / input.Length;
+                bucketinput[num].Add(input[i]);
+            }
+            for(int i=0;i<bucketnum;i++)
+            {
+                bucketinput[i].Sort();
+            }
+            int index = 0;
+            for(int i=0;i<bucketinput.Count;i++)
+            {
+                for(int j=0;j<bucketinput[i].Count;j++)
+                {
+                    input[index++] = bucketinput[i][j];
+                }
+            }
+        }
+    }
+}
---
+++ Sorting.Core/Yuyazhi/YYZQuickSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sorting.Core.Yuyazhi
+{
+    public class YYZQuickSort:SorterBase
+    {
+        public override void Sort(int[] input)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Sort(input, 0, input.Length - 1);
+        }
+        private static void Sort(int[] input,int leftindex,int rightindex)
+        {
+            int left = leftindex;
+            int right = rightindex;
+            if(left>=right)
+            {
+                return;
+            }
+            int basenum = input[left];
+            while(left!=right)
+            {
+                for(;left<right;right--)
+                {
+                    if (input[right] < basenum)
+                    {
+                        input[left] = input[right];
+                        break;
+                    }        
+                }
+                for(;left<right;left++)
+                {
+                    if(input[left]>basenum)
+                    {
+                        input[right] = input[left];
+                        break;
+                    }
+                }
+            }
+            input[left] = basenum;
+            Sort(input, leftindex, left - 1);
+            Sort(input, left + 1, rightindex);
+        }
+    }
+}
